3e28395fa571f49f5a17820ceb72464262101812
Jonah Lysne <jonahboy03@gmail.com>
2025-05-03 15:52:27 -0700

    Adds virtual memory code

---
+++ virtual_memory/SecondChance.java
@@ -0,0 +1,8 @@
+package virtual_memory;
+
+public class SecondChance {
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ virtual_memory/ThirdChance.java
@@ -0,0 +1,8 @@
+package virtual_memory;
+
+public class ThirdChance {
+    
+    public static void main(String[] args) {
+
+    }
+}
